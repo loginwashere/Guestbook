ca08c50a42645782cb92f1a8a2759c87a4f30042
Dmitry Savchenko <login.was.here@gmail.com>
2011-06-09 16:19:18 +0300

    Layout changed and some plaseholders added

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,6 +2,28 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
+	/**
+     * Enter description here ...
+     */
+    protected function _initPlaceholders()
+    {
+        $this->bootstrap('view');
+        $view = $this->getResource('view');
+
+        $view->doctype('HTML5');
+
+        $view->headMeta()->setCharset('UTF-8');
+
+        // Set the initial title and separator:
+        $view->headTitle("Don't panic")
+             ->setSeparator(' :: ');
+
+        // Set the initial stylesheet:
+        $view->headLink()->prependStylesheet('/css/global.css');
+
+        //set page title;
+        $view->title = "Don't panic";
+    }
 
 
 }
