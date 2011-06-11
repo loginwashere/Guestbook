fd179ff2b9306ed9dafd369c38b562886a55952c
Dmitry Savchenko <login.was.here@gmail.com>
2011-06-11 22:51:03 +0300

    Abstract classes added, some methods extracted to them

--- application/models/Guestbook.php
+++ application/models/Guestbook.php
@@ -1,6 +1,6 @@
 <?php
 
-class Application_Model_Guestbook
+class Application_Model_Guestbook extends Application_Model_GeustbookAbstract
 {
     protected $_comment;
     protected $_created;
@@ -8,43 +8,6 @@ class Application_Model_Guestbook
     protected $_email;
     protected $_username;
     protected $_id;
-
-    public function __construct(array $options = null)
-    {
-        if (is_array($options)) {
-            $this->setOptions($options);
-        }
-    }
-
-    public function __set($name, $value)
-    {
-        $method = 'set' . $name;
-        if (('mapper' == $name) || !method_exists($this, $method)) {
-            throw new Exception('Invalid guestbook property');
-        }
-        $this->$method($value);
-    }
-
-    public function __get($name)
-    {
-        $method = 'get' . $name;
-        if (('mapper' == $name) || !method_exists($this, $method)) {
-            throw new Exception('Invalid guestbook property');
-        }
-        return $this->$method();
-    }
-
-    public function setOptions(array $options)
-    {
-        $methods = get_class_methods($this);
-        foreach ($options as $key => $value) {
-            $method = 'set' . ucfirst($key);
-            if (in_array($method, $methods)) {
-                $this->$method($value);
-            }
-        }
-        return $this;
-    }
 
     public function setComment($text)
     {
--- application/models/GuestbookMapper.php
+++ application/models/GuestbookMapper.php
@@ -1,29 +1,7 @@
 <?php
 
-class Application_Model_GuestbookMapper
+class Application_Model_GuestbookMapper extends Application_Model_GeustbookMapperAbstract
 {
-    protected $_dbTable;
-
-    public function setDbTable($dbTable)
-    {
-        if (is_string($dbTable)) {
-            $dbTable = new $dbTable();
-        }
-        if (!$dbTable instanceof Zend_Db_Table_Abstract) {
-            throw new Exception('Invalid table data gateway provided');
-        }
-        $this->_dbTable = $dbTable;
-        return $this;
-    }
-
-    public function getDbTable()
-    {
-        if (null === $this->_dbTable) {
-            $this->setDbTable('Application_Model_DbTable_Guestbook');
-        }
-        return $this->_dbTable;
-    }
-
     public function save(Application_Model_Guestbook $guestbook)
     {
         $data = array(
