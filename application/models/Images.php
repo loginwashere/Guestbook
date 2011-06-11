446dfad51a7611db49a43fb4e6186d8392d1dffd
Dmitry Savchenko <login.was.here@gmail.com>
2011-06-11 22:52:23 +0300

    Images model created

---
+++ application/models/Images.php
@@ -0,0 +1,78 @@
+<?php
+
+class Application_Model_Images extends Application_Model_GeustbookAbstract
+{
+    protected $_id;
+    protected $_filename;
+    protected $_width;
+    protected $_height;
+    protected $_resizedwidth;
+    protected $_resizedheight;
+
+    public function setId($id)
+    {
+        $this->_id = (int) $id;
+        return $this;
+    }
+
+    public function getId()
+    {
+        return $this->_id;
+    }
+
+    public function setFilename($filename)
+    {
+        $this->_filename = (string) $filename;
+        return $this;
+    }
+
+    public function getFilename()
+    {
+        return $this->_filename;
+    }
+
+    public function setWidth($width)
+    {
+        $this->_width = (int) $width;
+        return $this;
+    }
+
+    public function getWidth()
+    {
+        return $this->_width;
+    }
+
+    public function setHeght($height)
+    {
+        $this->_height = (int) $height;
+        return $this;
+    }
+
+    public function getHeight()
+    {
+        return $this->_height;
+    }
+
+    public function setResizedwidth($resizedwidth)
+    {
+        $this->_resizedwidth = (int) $resizedwidth;
+        return $this;
+    }
+
+    public function getResizedwidth()
+    {
+        return $this->_resizedwidth;
+    }
+
+    public function setResizedheight($resizedheight)
+    {
+        $this->_resizedheight = (int) $resizedheight;
+        return $this;
+    }
+
+    public function getResizedheight()
+    {
+        return $this->_resizedheight;
+    }
+}
+
---
+++ application/models/ImagesMapper.php
@@ -0,0 +1,8 @@
+<?php
+
+class Application_Model_ImagesMapper extends Application_Model_GeustbookMapperAbstract
+{
+
+
+}
+
