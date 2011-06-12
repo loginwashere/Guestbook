7f4b59b7fdc1062195ff4c749dd507fdb119e201
Dmitry Savchenko <login.was.here@gmail.com>
2011-06-12 09:47:53 +0300

    Changes in db structute added to gb4nix.sql

--- gb4nix.sql
+++ gb4nix.sql
@@ -3,11 +3,26 @@
 --
 
 CREATE TABLE IF NOT EXISTS `guestbook` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `gid` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(30) NOT NULL DEFAULT 'Anonymous',
   `email` varchar(121) DEFAULT NULL,
   `url` varchar(60) DEFAULT NULL,
   `comment` text,
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=11 ;
+  PRIMARY KEY (`gid`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8
+
+--
+-- Table structure for table `images`
+--
+
+CREATE TABLE IF NOT EXISTS `images` (
+  `iid` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `filename` varchar(255) NOT NULL,
+  `width` smallint(5) unsigned NOT NULL,
+  `height` smallint(5) unsigned NOT NULL,
+  `resizedwidth` smallint(5) unsigned NOT NULL,
+  `resizedheight` smallint(5) unsigned NOT NULL,
+  `commentid` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`iid`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8
