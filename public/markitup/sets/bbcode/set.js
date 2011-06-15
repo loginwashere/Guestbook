b0eed5c84be365a9a45517edefb83421546de1b2
Dmitry Savchenko <login.was.here@gmail.com>
2011-06-15 16:44:03 +0300

    Some markitup standart options changed to options that work with standart Zend_Markup options

--- public/markitup/sets/bbcode/set.js
+++ public/markitup/sets/bbcode/set.js
@@ -12,25 +12,14 @@
 mySettings = {
 	previewParserPath:	'', // path to your BBCode parser
 	markupSet: [
-		{name:'Bold', key:'B', openWith:'[b]', closeWith:'[/b]'},
-		{name:'Italic', key:'I', openWith:'[i]', closeWith:'[/i]'},
-		{name:'Underline', key:'U', openWith:'[u]', closeWith:'[/u]'},
+		{name:'Strong', key:'S', openWith:'[strong]', closeWith:'[/strong]'},
+		{name:'Italic', key:'I', openWith:'[italic]', closeWith:'[/italic]'},
+		{name:'Underline', key:'U', openWith:'[ins]', closeWith:'[/ins]'},
 		{separator:'---------------' },
 		{name:'Picture', key:'P', replaceWith:'[img][![Url]!][/img]'},
 		{name:'Link', key:'L', openWith:'[url=[![Url]!]]', closeWith:'[/url]', placeHolder:'Your text to link here...'},
 		{separator:'---------------' },
-		{name:'Size', key:'S', openWith:'[size=[![Text size]!]]', closeWith:'[/size]',
-		dropMenu :[
-			{name:'Big', openWith:'[size=200]', closeWith:'[/size]' },
-			{name:'Normal', openWith:'[size=100]', closeWith:'[/size]' },
-			{name:'Small', openWith:'[size=50]', closeWith:'[/size]' }
-		]},
-		{separator:'---------------' },
-		{name:'Bulleted list', openWith:'[list]\n', closeWith:'\n[/list]'},
-		{name:'Numeric list', openWith:'[list=[![Starting number]!]]\n', closeWith:'\n[/list]'}, 
-		{name:'List item', openWith:'[*] '},
-		{separator:'---------------' },
-		{name:'Quotes', openWith:'[quote]', closeWith:'[/quote]'},
+		{name:'Quotes', openWith:'[cite]', closeWith:'[/cite]'},
 		{name:'Code', openWith:'[code]', closeWith:'[/code]'}, 
 		{separator:'---------------' },
 		{name:'Clean', className:"clean", replaceWith:function(markitup) { return markitup.selection.replace(/\[(.*?)\]/g, "") } },
